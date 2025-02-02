e64cd73cde1a474c81715a8c27ef730f7790d832
tahajaiti <kyojin.rar@gmail.com>
2025-02-02 16:43:07 +0100

    FEAT: designed the containers that gonna hold tasks with dynamic color display

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Lane from './Lane';
 
 const Container: React.FC = () => {
-  return (
-    <main className='min-h-[90vh] max-h-[90vh] grid grid-cols-1 lg:grid-cols-3 overflow-x-hidden'>
-        <Lane name='todo'/>
-        <Lane name='doing'/>
-        <Lane name='done'/>
-    </main>
-  )
+    return (
+        <main className='min-h-[90vh] max-h-[90vh] p-4 gap-4 grid grid-cols-1 lg:grid-cols-3 overflow-x-hidden'>
+            <Lane name='todo' />
+            <Lane name='doing' />
+            <Lane name='done' />
+        </main>
+    )
 }
 
 export default Container
--- frontend/src/components/Lane.tsx
+++ frontend/src/components/Lane.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { FaSort } from 'react-icons/fa'
 
 interface laneProps {
-    name: string
+    name: 'todo' | 'doing' | 'done'
 }
 
-const Lane: React.FC<laneProps> = ({name}) => {
-  return (
-    <div>Lane</div>
-  )
+const Lane: React.FC<laneProps> = ({ name }) => {
+    const colorCount = name === 'todo' ? 'bg-blue-300'
+        : name === 'doing' ? 'bg-purple-300' : 'bg-green-300';
+
+
+    return (
+        <div className='h-[40rem] bg-gray-500 rounded-sm border border-gray-300'>
+            <header className='bg-white flex justify-between items-center text-black p-2'>
+                <p className={`${colorCount} px-2 rounded-full`}>0</p>
+                <h2>{name.toUpperCase()}</h2>
+                <FaSort />
+            </header>
+        </div>
+    )
 }
 
 export default Lane
